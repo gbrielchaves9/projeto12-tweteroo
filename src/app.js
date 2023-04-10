efb811b9f65b81c6fd7fc26b7cb6eb9b3edbbd8d
Gabriel <gbrielchaves9@gmail.com>
2023-04-10 16:27:46 -0300

    login ok

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
 import express from "express";
 import cors from 'cors'
 
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const usuario =[{
+	username: 'bobesponja', 
+	avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" 
+}];
+
+app.post('/sign-up' , (req , res)=>{
+    const {username , avatar } = req.body;
+
+    usuario.push({username , avatar})
+
+    res.send("OK")
+})
+
+app.listen(5000)
