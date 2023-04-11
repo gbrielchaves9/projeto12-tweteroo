214350dd8dc2cded35eaf95cdd5679b2f41b914e
Gabriel <gbrielchaves9@gmail.com>
2023-04-11 13:26:48 -0300

    testar junto com o front

--- src/app.js
+++ src/app.js
@@ -18,17 +18,23 @@ app.post('/sign-up' , (req , res)=>{
 app.post('/tweets' , (req , res)=>{
    const twee =req.body
    const verificaLogin  = usuario.find(cadastro => cadastro.username === twee.username)
-   if(!verificaLogin) return res.send("UNAUTHORIZED")
+   if(!verificaLogin) res.send("UNAUTHORIZED")
 
    msgs.push(twee)
-    res.send("OK")
+    res.send("Ok")
 })
 
-app.post('/tweets' , (req , res)=>{
-  
- })
-/*app.get('/tweets' , (req , res)=>{
-    const pegaMsg =msgs.map(twee=> {
+app.get('/tweets' , (req , res)=>{
+
+
+    /*busca a msg do usuario dentro da lista dos usuarios quardando com o find dentro do objeto */
+    const PegaMsg = msgs.map((msg)=>{
+        const pessoa =usuario.find((e)=>e.username === msg.username)
+        return {...msg ,avatar : pessoa.avatar}
+
     })
-})*/
-app.listen(5000)
+    res.send(PegaMsg.slice-10)
+    /*slice -10 esta pegando os ultimos as ultimas 10 msgs !*/
+ })
+const PORT = 5000
+app.listen(PORT)
